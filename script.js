f15c3b4af83b4d8f6f4fcbd94ef4291f2f7641a0
IT23231528chamudi <IT23231528@my.sliit.lk>
2025-01-19 02:38:23 +0530

    All Done

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayMessage() {
+    const message = document.getElementById('message');
+    message.textContent = "Thank you for reaching out! I'll get back to you soon.";
+    message.style.color = "green";
+}
